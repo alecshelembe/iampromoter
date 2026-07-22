7c6c8326aaf8f89dc26c16f1706f8eb322191f8f
alec <alecshelembe@gmail.com>
2026-07-22 13:43:46 +0200

    nority blade updated

--- resources/views/mobile/notify.blade.php
+++ resources/views/mobile/notify.blade.php
@@ -35,7 +35,7 @@
 				 <div class="relative z-0 w-full mb-5 group">
 	           			 <label for="options" class="block text-gray-700 mb-2">Select Device</label>
 	               			   <select id="options" name="device" class=" focus:ring-blue-300 font-medium rounded-lg  w-full px-5 py-2.5 text-left dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800 block border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-blue-500 focus:border-blue-500 sm:">
-					    <option value="All">All Devices</option>
+					    <option value="All">-All Unavalable - use terminal command)</option>
     
 						    @foreach($devices as $device)
 						        <option value="{{ $device->expo_push_token }}">
